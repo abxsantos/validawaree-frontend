71a81bdda075985d33712bbb86667e40ef1281df
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-06-29 15:09:21 -0300

    Refactored code, removing unused files

---
+++ src/actions/types.js
@@ -0,0 +1,5 @@
+// action types
+export const INC_ROW = 'INC_ROW';
+export const INC_COLUMN = 'INC_COLUMN';
+export const UPD_SAMPLE_VALUE = 'UPD_SAMPLE_VALUE';
+export const UPD_LINEARITY_RESULT = 'UPD_LINEARITY_RESULT';
--- src/index.js
+++ src/index.js
@@ -2,22 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from './reducers'
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+  <Provider store={createStore(reducers, composeWithDevTools())}>
     <App />
   </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
