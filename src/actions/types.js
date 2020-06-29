4a11052bb2fce587e34e5ddcc28f22d147091564
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-06-29 15:11:59 -0300

    Merge with refactor/refactor-structure

--- src/actions/types.js
+++
@@ -1,5 +0,0 @@
-// action types
-export const INC_ROW = 'INC_ROW';
-export const INC_COLUMN = 'INC_COLUMN';
-export const UPD_SAMPLE_VALUE = 'UPD_SAMPLE_VALUE';
-export const UPD_LINEARITY_RESULT = 'UPD_LINEARITY_RESULT';
