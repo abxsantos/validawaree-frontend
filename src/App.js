7e8ca784edd1b72e4e747f5bd907beba2fe689a6
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-07-04 05:05:27 -0300

    Moved the calculate linearity button to the table component and changed style button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 
 import LinearitySampleInputTable from './Linearity/components/tables/LinearitySampleInputTable';
-import CalculateLinearityButton from './Linearity/components/button/CalculateLinearityButton';
 import LinearityRegressionAnovaTable from './Linearity/components/tables/LinearityRegressionAnovaTable';
 import LinearityRegressionChart from './Linearity/components/charts/LinearityRegressionGraph';
 import LinearityResiduesChart from './Linearity/components/charts/LinearityResiduesGraph';
@@ -11,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <LinearitySampleInputTable />
-      <CalculateLinearityButton />
       <LinearityRegressionAnovaTable />
       <LinearityRegressionChart />
       <LinearityResiduesChart />
