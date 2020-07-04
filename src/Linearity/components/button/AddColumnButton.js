e606f3b20d894203b90dd804733ec3f770af5790
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-07-04 03:38:31 -0300

    Changed add column button to icon button

--- src/Linearity/components/button/AddColumnButton.js
+++ src/Linearity/components/button/AddColumnButton.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import Tooltip from '@material-ui/core/Tooltip';
 
-import { incColumn } from '../../actions'
+import { incColumn } from '../../actions';
 
-const AddColumnButton = ({incColumn}) => {
+const AddColumnButton = ({ incColumn }) => {
   return (
-    <Button variant="contained" onClick={incColumn}>Add Column</Button>
+    <Tooltip
+      title='Add column'
+      placement='right'
+      arrow
+      disableFocusListener
+      disableTouchListener
+    >
+      <IconButton onClick={incColumn}>
+        <AddBoxIcon></AddBoxIcon>
+      </IconButton>
+    </Tooltip>
   );
-}
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    incColumn: () => {dispatch(incColumn())}
-  }
-}
+    incColumn: () => {
+      dispatch(incColumn());
+    },
+  };
+};
 
 export default connect(null, mapDispatchToProps)(AddColumnButton);
