277bb4fd875c445b3c2bdd4cdb12c5f0984cad3e
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-07-16 02:29:31 -0300

    Modified the dimensions of linearity result components

---
+++ src/components/Linearity/LinearityResutBlock.jsx
@@ -0,0 +1,52 @@
+import React, { Fragment } from "react";
+
+import Grid from "@material-ui/core/Grid";
+import Fade from "react-reveal";
+
+import LinearityRegressionAnovaTable from "./components/tables/LinearityRegressionAnovaTable";
+import LinearityDataStatisticsTable from "./components/tables/LinearityDataStatisticsTable";
+import LinearityCoefficientsTable from "./components/tables/LinearityCoefficientsTable";
+import LinearityRegressionGraph from "./components/charts/LinearityRegressionGraph";
+import LinearityResiduesChart from "./components/charts/LinearityResiduesGraph";
+
+function LinearityResultBlock() {
+  return (
+    <>
+      <Fade left cascade duration={1000} delay={500} distance="30px">
+        <h2>Linearity Results</h2>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={3}
+        >
+          <Grid item xs={8}>
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+            >
+              <LinearityRegressionGraph />
+              <LinearityRegressionAnovaTable />
+            </Grid>
+          </Grid>
+          <Grid item xs={4}>
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+            >
+              <LinearityResiduesChart />
+              <LinearityDataStatisticsTable />
+              <LinearityCoefficientsTable />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Fade>
+    </>
+  );
+}
+export default LinearityResultBlock;
--- src/components/Linearity/components/tables/LinearityCoefficientsTable.jsx
+++ src/components/Linearity/components/tables/LinearityCoefficientsTable.jsx
@@ -1,37 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import { connect } from 'react-redux';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import TableContainer from "@material-ui/core/TableContainer";
+import { makeStyles } from "@material-ui/core/styles";
 
+import { connect } from "react-redux";
+const useStyles = makeStyles({
+  table: {
+    maxWidth: 650,
+    maxHeight: 300,
+  },
+});
 function LinearityCoefficientsTable(props) {
+  const classes = useStyles();
+
   return (
-    <Table size='small' aria-label='a dense table'>
-      <TableHead>
-        <TableRow>
-          <TableCell align='center'>{'Intercept'}</TableCell>
-          <TableCell align='center'>{'Slope'}</TableCell>
-          <TableCell align='center'>{'R²'}</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        <TableRow key={'data-statistics'}>
-          <TableCell align='center'>{props.intercept}</TableCell>
-          <TableCell align='center'>{props.slope}</TableCell>
-          <TableCell align='center'>{props.rSquared}</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <TableContainer className={classes.table} component={Paper}>
+      <Table size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">{"Intercept"}</TableCell>
+            <TableCell align="center">{"Slope"}</TableCell>
+            <TableCell align="center">{"R²"}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow key={"data-statistics"}>
+            <TableCell align="center">{props.intercept}</TableCell>
+            <TableCell align="center">{props.slope}</TableCell>
+            <TableCell align="center">{props.rSquared}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
 const mapStateToProps = function (state) {
   return {
     intercept: state.linearity.regressionCoefficients.intercept,
-    insignificantIntercept: state.linearity.regressionCoefficients.insignificant_intercept,
+    insignificantIntercept:
+      state.linearity.regressionCoefficients.insignificant_intercept,
     slope: state.linearity.regressionCoefficients.slope,
     significantSlope: state.linearity.regressionCoefficients.significant_slope,
     rSquared: state.linearity.regressionCoefficients.r_squared,
