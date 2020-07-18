114aa59810c624f17db7cfab207ffe0092a4671a
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-07-17 21:56:45 -0300

    Moved the constants for better readability

--- src/App.test.js
+++ src/App.test.js
@@ -4,13 +4,11 @@ import { shallow } from "enzyme";
 import App from "./App";
 import HomePage from "./screens/HomePage/HomePage";
 import LinearityPage from "./screens/LinearityPage/LinearityPage";
-import Footer from "./components/Footer";
-
 
 describe("App component", () => {
   describe("Must render page wrapper components", () => {
+    const wrapper = shallow(<App />);
     it("Should render Scroll to top without errors.", () => {
-      const wrapper = shallow(<App />);
       const scrollToTopCompoment = wrapper.find(".scrollToTopComponent");
       expect(scrollToTopCompoment.exists()).toBe(true);
     });
@@ -22,7 +20,6 @@ describe("App component", () => {
       `(
         "When the user enters the $expectedPath path the component $expectedComponent at the route $expectedRouteClassName should be rendered",
         ({ expectedPath, expectedRouteClassName, expectedComponent }) => {
-          const wrapper = shallow(<App />);
           const route = wrapper.find(expectedRouteClassName).prop("path");
           const routeComponent = wrapper
             .find(expectedRouteClassName)
@@ -31,11 +28,10 @@ describe("App component", () => {
           expect(() => routeComponent.toBe(expectedComponent));
         }
       );
-        it('The footer component should be rendered in the App main component', ()=>{
-            const wrapper = shallow(<App/>);
-            const footerComponent = wrapper.find(".footerComponent")
-            expect(footerComponent.exists()).toBe(true)
-        })
+      it("The footer component should be rendered in the App main component", () => {
+        const footerComponent = wrapper.find(".footerComponent");
+        expect(footerComponent.exists()).toBe(true);
+      });
     });
   });
 });
