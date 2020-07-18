f189b7bdae1370d3c2f01704437545f75efcad39
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-07-17 21:17:49 -0300

    Set the router to the component function

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import React, { Fragment } from "react"; // eslint-disable-line
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-
-import LinearityPage from "./screens/Linearity/LinearityPage";
+import LinearityPage from "./screens/LinearityPage/LinearityPage";
 import HomePage from "./screens/HomePage/HomePage";
 
 import ScrollToTop from "./components/ScrollToTop";
 import Footer from "./components/Footer";
-import "./App.css"
+import "./App.css";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <ScrollToTop>
-            <Route path="/" exact component={() => <HomePage />} />
+          <ScrollToTop className="scrollToTop">
             <Route
+              className="homePageRoute"
+              path="/"
+              exact
+              component={HomePage}
+            />
+            <Route
+            className="linearityPageRoute"
               path="/linearity"
               exact
-              component={() => <LinearityPage />}
+              component={LinearityPage}
             />
             <Footer />
           </ScrollToTop>
